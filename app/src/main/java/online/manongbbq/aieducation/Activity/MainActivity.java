75dccb5e4ad61108d04f0a73a4755c3f3be43128
梁国伟 <88544588@gitlab.com>
2024-07-08 22:21:16 +0800

    ui

--- app/src/main/java/online/manongbbq/aieducation/Activity/MainActivity.java
+++ app/src/main/java/online/manongbbq/aieducation/Activity/MainActivity.java
@@ -1,4 +1,4 @@
-package online.manongbbq.aieducation;
+package online.manongbbq.aieducation.Activity;
 
 import android.os.Bundle;
 
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import online.manongbbq.aieducation.R;
 
 public class MainActivity extends AppCompatActivity {
 
