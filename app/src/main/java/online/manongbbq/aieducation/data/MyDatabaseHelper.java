843a2984f71222d41980030b11f531121df619d5
danngo-sama <ly1026hj@outlook.com>
2024-07-08 23:52:42 +0800

    data包

---
+++ app/src/main/java/online/manongbbq/aieducation/data/MyDatabaseHelper.java
@@ -0,0 +1,47 @@
+package online.manongbbq.aieducation.data;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * This is the class to help build a database.
+ *
+ * @author liang zifan
+ * @version 0.0.1
+ * @since 07/08
+ */
+public class MyDatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "database.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public MyDatabaseHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        // 创建表
+        db.execSQL("CREATE TABLE userinfo (userid INTEGER PRIMARY KEY, password INTEGER, " +
+                "name TEXT, facedata BLOB, isstudent BOOLEAN)");
+        db.execSQL("CREATE TABLE classinfo (classid INTEGER PRIMARY KEY, coursename TEXT, " +
+                "coursedescription TEXT)");
+        db.execSQL("CREATE TABLE errorbook (bookid INTEGER PRIMARY KEY, errorimg BLOB, " +
+                "erroranalys TEXT)");
+        db.execSQL("CREATE TABLE leaveinfo (leaveid INTEGER PRIMARY KEY, leavecontent TEXT, " +
+                "leavestudentid INTEGER, leaveclassid INTEGER, leavedate DATE)");
+        db.execSQL("CREATE TABLE airequest (requestid INTEGER PRIMARY KEY, requestcontent TEXT, " +
+                "requestresult TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // 升级数据库（这里简单地删除旧表并创建新表）
+        db.execSQL("DROP TABLE IF EXISTS userinfo");
+        db.execSQL("DROP TABLE IF EXISTS classinfo");
+        db.execSQL("DROP TABLE IF EXISTS errorbook");
+        db.execSQL("DROP TABLE IF EXISTS leaveinfo");
+        db.execSQL("DROP TABLE IF EXISTS airequest");
+        onCreate(db);
+    }
+}
