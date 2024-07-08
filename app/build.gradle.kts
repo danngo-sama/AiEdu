e8c48b2e7833296aa604749714d5d3b511a08165
whm <haominwan818@gmail.com>
2024-07-08 21:57:32 +0800

    添加了alibaba okhttp3依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,6 +34,10 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+
+    implementation("com.alibaba:fastjson:1.2.78")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
