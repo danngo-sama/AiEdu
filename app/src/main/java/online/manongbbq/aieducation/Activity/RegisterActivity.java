b883845b7e57ee6b3dccf1b4fcb2d9707ab4c432
梁国伟 <88544588@gitlab.com>
2024-07-09 00:51:37 +0800

    ui

---
+++ app/src/main/java/online/manongbbq/aieducation/Activity/RegisterActivity.java
@@ -0,0 +1,16 @@
+package online.manongbbq.aieducation.Activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import online.manongbbq.aieducation.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+}
